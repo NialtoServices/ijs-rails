df7cd8a893dea2f6e74819ce07363512f59787ed
Toby Herbert <tobyherbert@protonmail.com>
2020-02-22 21:38:46 +0000

    Added additional config options.

--- lib/ijs-rails/railtie.rb
+++ lib/ijs-rails/railtie.rb
@@ -3,12 +3,15 @@
 raise 'ijs-rails is an extension for Rails.' unless defined?(Rails::Railtie)
 
 module IJSRails
-  # The main entry point which integrates this gem into the Rails system.
+  # The railtie which ties the gem into the Rails system.
   #
   class Railtie < Rails::Railtie
     initializer 'inline_js.set_configs' do |app|
       app.configure do
         config.inline_js = ActiveSupport::OrderedOptions.new
+        config.inline_js.path = Rails.root.join('app/javascript/inline')
+        config.inline_js.cache_path = Rails.root.join('tmp/ijs')
+        config.inline_js.isolate_scripts = true
       end
     end
   end
